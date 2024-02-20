8389c8ad40ff4612cf46cf28904bd34af7f78002
Brittany <bowlingb@oregonstate.edu>
2024-02-19 23:23:25 -0800

    Update clean_fastq.R

--- R/clean up/clean_fastq.R
+++ R/clean up/clean_fastq.R
@@ -15,10 +15,24 @@ library("phyloseq")
 library("DECIPHER")
 library("phangorn")
 
-# import data
+# import and group data
+# folder path to raw data
 miseq_path <- file.path("data", "raw", "mouse")
+# folder path for newly cleaned data
 filt_path <- file.path("data", "modified", "mouse")
+# list of all file names
 fns <- sort(list.files(miseq_path, full.names = TRUE))
+# forward sequence
+fnFs <- fns[grepl("R1", fns)]
+# reverse sequence
+fnRs <- fns[grepl("R2", fns)]
+
+# trim and filter sequences
+# needs to be modified after looking at actual data
+# plot quality of both forward and reverse reads
+ii <- sample(length(fnFs), 3)
+for(i in ii) {print(plotQualityProfile(fnFs[i]) + ggtitle("Forward"))}
+for(i in ii) {print(plotQualityProfile(fnRs[i]) + ggtitle("Reverse"))}
 
 # Diagnostic plot
 
