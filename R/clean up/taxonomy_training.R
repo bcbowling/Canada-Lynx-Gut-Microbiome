b15facd245055a8ff60a34efb216744770b2ece3
Brittany <bowlingb@oregonstate.edu>
2024-03-12 21:51:02 -0700

    Update taxonomy_training.R

--- R/clean up/taxonomy_training.R
+++ R/clean up/taxonomy_training.R
@@ -1,5 +1,10 @@
-# Using Ribosomal Database Project training set 19
-# Modify training set to format needed
+# modify training set to format needed for dada2 package
+# using Ribosomal Database Project training set 19
+
+# load required packages
+library("dada2")
+
+# create modified training file
 dada2:::makeTaxonomyFasta_RDP(file.path("data", "raw",
                                         "RDPTraining",
                                         "trainset19_072023.fa"),
