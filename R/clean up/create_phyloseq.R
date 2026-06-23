590d1c849e321afdbeadbf5d9c808e4d5697154c
Brittany <bowlingb@oregonstate.edu>
2026-06-23 13:02:31 -0700

    Update create_phyloseq.R

--- R/clean up/create_phyloseq.R
+++ R/clean up/create_phyloseq.R
@@ -4,35 +4,19 @@
 library("phyloseq")
 
 # load previously constructed components
-load(file = "data/modified/mouse/taxonomy_table.Rdata")
-load(file = "data/modified/mouse/sequence_table.Rdata")
-load(file = "data/modified/mouse/phylogenetic_tree.Rdata")
+load(file = "data/modified/zoolynx/intermediates/taxonomy_table.Rdata")
+load(file = "data/modified/zoolynx/intermediates/sequence_table.Rdata")
+load(file = "data/modified/zoolynx/intermediates/phylogenetic_tree.Rdata")
 
 # upload csv data
-mimarks_path <- "data/raw/mouse/MIMARKS_Data_combined.csv"
-samdf <- read.csv(mimarks_path, header = TRUE)
-
-# modify csv
-# will need to adjust for specific csv
-samdf$sample_id <- paste0(gsub("00", "", samdf$host_subject_id),
-                          "D", samdf$age - 21)
-# remove duplicate entries
-samdf <- samdf[!duplicated(samdf$sample_id), ]
-# fixing an error with this dataset
-rownames(seqtab) <- gsub("124", "125", rownames(seqtab))
+metadata_path <- "data/raw/zoolynx/ZooExperiment_Metadata.csv"
+samdf <- read.csv(metadata_path, header = TRUE)
 
 # check if csv id's match sequence table id's (should return TRUE)
-all(rownames(seqtab) %in% samdf$sample_id)
+all(rownames(seqtab) %in% samdf$SampleID)
 
-# continue modifying csv
-# columns to keep will depend on csv data
-rownames(samdf) <- samdf$sample_id
-keep_cols <- c("collection_date", "biome", "target_gene",
-               "target_subfragment", "host_common_name",
-               "host_subject_id", "age", "sex", "body_product",
-               "tot_mass", "diet", "family_relationship",
-               "genotype", "sample_id")
-samdf <- samdf[rownames(seqtab), keep_cols]
+# make sample IDs the row names
+rownames(samdf) <- samdf$SampleID
 
 # create phyloseq object combining all previous components
 ps <- phyloseq(tax_table(taxtab), sample_data(samdf),
@@ -40,5 +24,5 @@ ps <- phyloseq(tax_table(taxtab), sample_data(samdf),
                phy_tree(fit_gtr$tree))
 
 # save phyloseq object
-filename <- "data/modified/mouse/combined_data.Rdata"
+filename <- "data/modified/zoolynx/intermediates/combined_data.Rdata"
 save(ps, file = filename)
